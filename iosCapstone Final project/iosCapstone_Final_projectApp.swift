1e832c5bd41a881823815e5eea0a417c838824f7
Rueben <akankobatengr@gmail.com>
2023-05-26 15:23:30 +0000

    Initial Commit

---
+++ iosCapstone Final project/iosCapstone_Final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosCapstone_Final_projectApp.swift
+//  iosCapstone Final project
+//
+//  Created by Rueben on 26/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct iosCapstone_Final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
